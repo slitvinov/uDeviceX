98dd4122063dcac21c3b689878cdbbecd5f2b5c3
Sergey Litvinov <slitvinov@gmail.com>
2017-04-27 20:49:17 +0200

    Update

--- tools/efit/efit.m
+++ tools/efit/efit.m
@@ -2,6 +2,8 @@
 
 fn = '../test_data/rbc.ply'
 
+X = 1; Y = 2; Z = 3;
+
 fd = fopen(fn); nl  = @() fgetl(fd); % next line
 nl(); nl();
 l = nl(); nv = sscanf(l, 'element vertex %d');
@@ -13,5 +15,37 @@ nvar  = 6; % x, y, z, u, v, w
 D = fread(fd, [nvar     , nv], 'float32');
 D = D(1:3, :);
 xx = D(1, :);  yy = D(2, :);  zz = D(3, :);
+fclose(fd);
 
-[center, radii, evecs] = ellipsoid_fit(D')
+[center, radii, evecs] = ellipsoid_fit(D');
+a = radii(1); b = radii(2); c = radii(3);
+
+ndump = 100;
+uu = linspace(0, 2*pi, 100);
+vv = linspace(0,   pi, 100);
+[uu, vv] = meshgrid (uu, vv);
+
+n = numel(uu);
+xx = zeros(1, n); yy = zeros(1, n); zz = zeros(1, n); 
+
+for i = 1:numel(uu)
+  u = uu(i); v = vv(i);
+  x = a*cos(u)*sin(v); % u: [0, pi]
+  y = b*sin(u)*sin(v); % v: [0, pi]
+  z = c*cos(v);
+
+  r = [x, y, z]';
+  r = evecs * r;
+  r = r + center;
+  
+  xx(i) = r(X); yy(i) = r(Y); zz(i) = r(Z);
+end
+
+
+disp("writing: e.3d");
+fd = fopen("e.3d", "w");
+fdisp(fd, "x y z sc");
+sc = zeros(1, n); % fake scalar
+dlmwrite(fd, [xx; yy; zz; sc]', ' ');
+
+fclose(fd);
