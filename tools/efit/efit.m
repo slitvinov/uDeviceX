375cf25c961d40c862702f98ed09fae226fc5bc6
Sergey Litvinov <slitvinov@gmail.com>
2017-04-27 20:18:02 +0200

    Update

--- tools/efit/efit.m
+++ tools/efit/efit.m
@@ -1,6 +1,6 @@
 #!/usr/bin/env octave-qf
 
-fn = 'rbc.org.ply'
+fn = '../test_data/rbc.ply'
 
 fd = fopen(fn); nl  = @() fgetl(fd); % next line
 nl(); nl();
@@ -9,6 +9,7 @@ nl(); nl(); nl(); nl(); nl(); nl();
 l = nl(); nf = sscanf(l, 'element face %d');
 nl(); nl();
 
+nvar  = 6; % x, y, z, u, v, w
 D = fread(fd, [nvar     , nv], 'float32');
 D = D(1:3, :);
 xx = D(1, :);  yy = D(2, :);  zz = D(3, :);
