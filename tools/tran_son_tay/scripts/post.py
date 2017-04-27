0aba2f990fb10a2541bf8d50983df1c706683e8b
lina-kulakova <lina.kulakova@mavt.ethz.ch>
2017-04-27 19:31:09 +0200

    Add asphericity (2d)

--- tools/tran_son_tay/scripts/post.py
+++ tools/tran_son_tay/scripts/post.py
@@ -1,60 +1,14 @@
-import numpy as np
-import glob
 from argparse import ArgumentParser
-from plyfile import PlyData
+from glob import glob
 from math import floor, atan, atan2
+import numpy as np
+import numpy.linalg as la
+from plyfile import PlyData
 from sklearn.decomposition import PCA
-import scipy.fftpack as scfft
-from scipy.optimize import curve_fit
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 from pylab import savefig
-
-
-def read_data(plydir, dt, ntspd):
-    listing = glob.glob(plydir+"/rbcs-*.ply"); listing.sort()
-    nfiles = len(listing)
-
-    t = dt*ntspd*np.arange(nfiles)  # DPD t
-    start = nfiles-int(floor(0.5*nfiles))  # Length of signal
-    end = min(start+2e6, nfiles)  # Length of signal
-    t = t[start:end]
-    ls = end-start
-
-    # find marker
-    fullpath = listing[0]
-    x,y,z = read_ply_file(PlyData.read(fullpath))
-    midx = np.argmax(x)  # the rightmost point will be a marker
-    a0 = np.max(x)-np.min(x)
-    c0 = np.max(y)-np.min(y)
-
-    th = np.zeros((ls,))  # angle with the projection on Ox
-    om = np.zeros((ls,))  # angle of the marker with the current RBC axis
-    diam_a = np.zeros((ls,))
-    diam_c = np.zeros((ls,))
-
-    for i in range(ls):
-        fullpath = listing[start+i]
-        x,y,z = read_ply_file(PlyData.read(fullpath))
-        x = x-np.mean(x); y = y-np.mean(y); z = z-np.mean(z)
-
-        th[i] = get_th(x, y, z)
-        om[i] = get_om(x, y, z, midx, th[i])
-        diam_a[i] = (np.max(x)-np.min(x))/a0
-        diam_c[i] = (np.max(y)-np.min(y))/c0
-
-        if i % 100 == 0: print 'Computed up to', i, '/', ls
-
-    save_res('Tran-Son-Tay_result.txt', (t,th,om,diam_a,diam_c))
-    plt.plot(t, om, 'b-'); plt.plot(t, th, 'r-')
-    savefig('Tran-Son-Tay_angle.png')
-    plt.close()
-    plt.plot(t, diam_a, 'b-'); plt.plot(t, diam_c, 'r-')
-    savefig('Tran-Son-Tay_diam.png')
-    plt.close()
-
-    return t, th, om, diam_a, diam_c
 
 
 def wrapTo(x,left,right):
@@ -68,6 +22,91 @@ def read_ply_file(ply):
     vertex = ply['vertex']
     x,y,z = (vertex[p] for p in ('x', 'y', 'z'))
     return x,y,z
+
+
+def save_res(filename, arr_in):
+    arr = ()
+    for a in arr_in:
+        arr = arr + (a.reshape((len(a),1)),)
+    np.savetxt(filename, np.concatenate(arr,axis=1), fmt='%.6e', delimiter=' ')
+
+
+def read_data(plydir, dt, ntspd):
+    listing = glob(plydir+"/rbcs-*.ply"); listing.sort()
+    nfiles = len(listing)
+
+    t = dt*ntspd*np.arange(nfiles)  # DPD t
+    start = nfiles-int(floor(0.5*nfiles))  # Length of signal
+    end = min(start+2e6, nfiles)  # Length of signal
+    t = t[start:end]
+    ls = end-start
+
+    # find marker
+    fullpath = listing[0]
+    x, y, z = read_ply_file(PlyData.read(fullpath))
+    midx = np.argmax(x)  # the rightmost point will be a marker
+    a0 = np.max(x)-np.min(x)
+    c0 = np.max(y)-np.min(y)
+
+    th = np.zeros((ls,))  # angle with the projection on Ox
+    om = np.zeros((ls,))  # angle of the marker with the current RBC axis
+    el = np.zeros((ls,))
+    a  = np.zeros((ls,))
+    c  = np.zeros((ls,))
+
+    for i in range(ls):
+        fullpath = listing[start+i]
+        x, y, z = read_ply_file(PlyData.read(fullpath))
+        x -= np.mean(x); y -= np.mean(y); z -= np.mean(z)
+
+        th[i] = get_th(x, y, z)
+        om[i] = get_om(x, y, z, midx, th[i])
+        el[i] = get_el(x, y, z)
+        a[i]  = (np.max(x)-np.min(x))/a0
+        c[i]  = (np.max(y)-np.min(y))/c0
+
+        if i % 100 == 0: print 'Computed up to', i, '/', ls
+
+    save_res('result.txt', (t,th,om,a,c))
+    plt.plot(t, om, 'b-'); plt.plot(t, th, 'r-')
+    savefig('angle.png')
+    plt.close()
+    plt.plot(t, a, 'b-'); plt.plot(t, c, 'r-')
+    savefig('diam.png')
+    plt.close()
+
+    return t, th, om, a, c, el
+
+
+# find the distance from the ellipse
+def get_el(x, y, z):
+    n = len(x)
+    xyz = np.array([x, y, z])
+    e, v = get_I(xyz)
+    xyz_new = np.asarray(np.matrix(la.inv(v))*np.matrix(xyz))
+    nx = np.squeeze(xyz_new[0, :]); ny = np.squeeze(xyz_new[2, :])
+    en = 100; ex = np.zeros(en); ey = np.zeros(en)
+    for i in range(en):
+        a = np.max(nx); b = np.max(ny)
+        phi = 2*np.pi*i/en
+        r = a*b/np.sqrt((a*np.sin(phi))**2 + (b*np.cos(phi))**2)
+        ex[i] = r*np.cos(phi); ey[i] = r*np.sin(phi)
+    plt.plot(nx, ny, 'bo'); plt.plot(ex, ey, 'ro'); plt.savefig('test.png')
+    d = 0
+    for i in range(n):
+        d += np.min([la.norm(np.array([nx[i], ny[i]])-np.array([ex[j], ey[j]])) for j in range(en)])
+    return d/n
+
+
+def get_I(data):
+    dim, n = data.shape
+    I = np.zeros((dim, dim))
+    for i in range(0, n):
+        ri = data[:, i]
+        I += np.dot(ri, ri)*np.identity(dim) - np.kron(ri, ri).reshape(dim, dim)
+    e, v = la.eig(I)
+    idx = np.argsort(e)
+    return e[idx], v[:, idx]
 
 
 # find the current axis
@@ -86,13 +125,6 @@ def get_om(x,y,z,id,th):
     res = wrapTo(np.rad2deg(res), -180, 180)
     res = wrapTo(th-res, -180, 180)
     return res
-
-
-def save_res(filename, arr_in):
-    arr = ()
-    for a in arr_in:
-        arr = arr + (a.reshape((len(a),1)),)
-    np.savetxt(filename, np.concatenate(arr,axis=1), fmt='%.6e', delimiter=' ')
 
 
 def get_fr(x, y):
@@ -114,7 +146,7 @@ def get_fr(x, y):
 
     plt.plot(x, y, 'b-o')
     plt.plot(x[peakind], y[peakind], 'ro')
-    plt.savefig('Tran-Son-Tay_peaks.png')
+    plt.savefig('peaks.png')
     plt.close()
 
     return best[0], best[1]/best[0]
@@ -136,13 +168,14 @@ if __name__ == '__main__':
     ntspd  = int(args.st)  # number of t steps per dump
     dt     = float(args.dt)
 
-    t, th, om, a, c = read_data(plydir, dt, ntspd)
+    t, th, om, a, c, el = read_data(plydir, dt, ntspd)
     a, au = np.mean(a), np.std(a)
     c, cu = np.mean(c), np.std(c)
+    el, elu = np.mean(el), np.std(el)
     th, thu = get_an(th)
     fr, fru = get_fr(t, om)
     ish = 1./sh; fr *= 2.*np.pi*ish; fru *= ish
 
     with open('post.txt', 'w') as f:
-        f.write('# fr fru a au c cu th thu\n')
-        f.write('%g %g %g %g %g %g %g %g\n' % (fr, fru, a, au, c, cu, th, thu))
+        f.write('# fr fru a au c cu th thu el elu\n')
+        f.write('%g %g %g %g %g %g %g %g %g %g\n' % (fr, fru, a, au, c, cu, th, thu, el, elu))
