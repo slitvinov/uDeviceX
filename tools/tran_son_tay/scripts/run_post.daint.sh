d20d0e9862419867751407e4648a3c771b2a6a4b
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2017-05-01 17:01:16 +0200

    Comment out some printing; output parameters when postprocessing

--- tools/tran_son_tay/scripts/run_post.daint.sh
+++ tools/tran_son_tay/scripts/run_post.daint.sh
@@ -1,11 +1,13 @@
-for d in $(seq 2 5); do
+for d in $(seq 27 117); do
     (
-    echo "Processing run $d"
     cd simulations/run_$d
-    cd ply && sh ~/scripts/ply/cm.sh && cd ..
     dt=$(awk '$1 == "dt"             {print $2}' params.txt)
     sh=$(awk '$1 == "_gamma_dot"     {print $2}' params.txt)
     st=$(awk '$1 == "steps_per_dump" {print $2}' params.txt)
-    python ~/rbc_uq/tools/tran_son_tay/scripts/post.py --dt=$dt --ply=ply --sh=$sh --st=$st
+    go=$(awk '$1 == "_gammadpd_out"  {print $2}' params.txt)
+    gi=$(awk '$1 == "_gammadpd_in"   {print $2}' params.txt)
+    gc=$(awk '$1 == "RBCgammaC"      {print $2}' params.txt)
+	echo "Processing run $d with parameters $go, $gi, $gc"
+    post.py --dt=$dt --ply=ply --sh=$sh --st=$st
     )
 done
