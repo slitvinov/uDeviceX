cdc5e0041888829fe56c16ac6f5707547eb153e7
Lina Kulakova <lina.kulakova@mavt.ehtz.ch>
2017-05-04 15:48:20 +0200

    Dump b, too

--- tools/tran_son_tay/scripts/post.py
+++ tools/tran_son_tay/scripts/post.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
-from argparse import ArgumentParser
-from glob import glob
-import numpy as np
-from numpy.linalg import norm
-from plyfile import PlyData
-from sklearn.decomposition import PCA
 import matplotlib as mpl
 mpl.use('Agg')
-import matplotlib.pyplot as plt
-from pylab import savefig
-import efit as ef
-import os
+import numpy as np
+
+from argparse import ArgumentParser
+from efit import fit_ellipsoid_ply
+from glob import glob
+from matplotlib.pyplot import plot, savefig, close, legend
+from numpy.linalg import norm
+from os import makedirs
+from os.path import exists
+from plyfile import PlyData
+from sklearn.decomposition import PCA
+
+
+verbose = 0
 
 
 def wrap(x,left,right):
@@ -20,11 +24,11 @@ def wrap(x,left,right):
     return x
 
 
-def save_res(filename, arr_in):
+def save_txt(filename, arr_in):
     arr = ()
     for a in arr_in:
-        arr = arr + (a.reshape((len(a),1)),)
-    np.savetxt(filename, np.concatenate(arr,axis=1), fmt='%.6e', delimiter=' ')
+        arr = arr + (a.reshape((len(a), 1)),)
+    np.savetxt(filename, np.concatenate(arr, axis=1), fmt='%.6e', delimiter=' ')
 
 
 def read_ply(fname):
@@ -63,84 +67,91 @@ def get_fr(x, y):
 
     if fru > cfru: fr = cfr; fru = cfru
 
-    plt.plot(x, y, 'b-o', label='theta')
-    plt.plot(x[peakind], y[peakind], 'ro', label='peaks')
-    plt.legend()
-    plt.savefig('peaks.png')
-    plt.close()
+    # plot
+    plot(x, y, 'b-o', label='theta')
+    plot(x[peakind], y[peakind], 'ro', label='peaks')
+    legend()
+    savefig('peaks.png')
+    close()
 
     return fr, fru/fr
 
 
 def process_data(plydir, dt, ntspd, sh):
     ed = 'e' # directory for ellipsoid dumps
-    if not os.path.exists(ed): os.makedirs(ed)
+    if not exists(ed): makedirs(ed)
     cd = 'c' # directory for COM dumps
-    if not os.path.exists(cd): os.makedirs(cd)
+    if not exists(cd): makedirs(cd)
 
     # initialization
     files = glob(plydir+"/rbcs-*.ply"); files.sort()
     n = len(files)
     th = np.zeros(n)  # angle with the projection on Ox
     om = np.zeros(n)  # angle of the marker with the current RBC axis
-    el = np.zeros(n)
-    a  = np.zeros(n)
-    b  = np.zeros(n)
-    c  = np.zeros(n)
-    a_ = np.zeros(n)
-    c_ = np.zeros(n)
+    el = np.zeros(n)  # ellipticity
+    a  = np.zeros(n); b  = np.zeros(n); c  = np.zeros(n)
+    a_ = np.zeros(n); b_ = np.zeros(n); c_ = np.zeros(n)
     ch = int(np.floor(n/20))
-    steady = 0; si = 0.75*n; ave = 0
+    steady = False; si = 0.75*n; ave = 0
+    A = 0; B = 2; C = 1  # x, y, z corresponding to a, b, c
 
     # main loop
     for i in range(n):
         fname = files[i]
-        center, rot, radii, chi2, xyz = ef.fit_ellipsoid_ply(fname,
+        center, rot, radii, chi2, xyz = fit_ellipsoid_ply(fname,
             '%s/%05d' % (cd, i), '%s/%05d' % (ed, i))
 
         if i == 0:
-            mi = np.argmax(xyz[:,0])  # the rightmost point will be a marker
-            a0 = radii[0]; b0 = radii[1]; c0 = radii[2]
-            a_0 = np.max(xyz[:,0]) - np.min(xyz[:,0])
-            c_0 = np.max(xyz[:,2]) - np.min(xyz[:,2])
+            mi = np.argmax(xyz[:,A])  # the rightmost point will be a marker
+            # a0 = radii[A]; b0 = radii[B]; c0 = radii[C]
+            a0 = np.max(xyz[:,A]) - np.min(xyz[:,A])
+            b0 = np.max(xyz[:,B]) - np.min(xyz[:,B])
+            c0 = np.max(xyz[:,C]) - np.min(xyz[:,C])
 
-        a[i] = radii[0]/a0; b[i] = radii[1]/b0; c[i] = radii[2]/c0
-        th[i] = get_angle_btw_vectors(rot[:,0], np.array([1,0,0]))
+        a[i] = 2*radii[A]/a0
+        b[i] = 2*radii[B]/b0
+        c[i] = 2*radii[C]/c0
+        a_[i] = (np.max(xyz[:,A]) - np.min(xyz[:,A]))/a0
+        b_[i] = (np.max(xyz[:,B]) - np.min(xyz[:,B]))/b0
+        c_[i] = (np.max(xyz[:,C]) - np.min(xyz[:,C]))/c0
+        th[i] = get_angle_btw_vectors(rot[:,A], np.array([1,0,0]))
         om[i] = get_om(fname, mi, th[i])
         el[i] = chi2
-        a_[i] = (np.max(xyz[:,0]) - np.min(xyz[:,0]))/a_0
-        c_[i] = (np.max(xyz[:,2]) - np.min(xyz[:,2]))/c_0
 
         # check whether we're in a steady state
         if ch > 0 and (i+1) % ch == 0:
             cur = np.mean(a[i-ch+1:i])
-            if not steady and abs(ave-cur) < 0.02*ave:
-                print "Steady state reached after", i, "steps out of", n
-                steady = 1; si = i
+            if not steady and np.abs(ave-cur) < 0.02*ave:
+                steady = True; si = i
+                if verbose: print 'Steady state reached after %d/%d steps' % (i, n)
             else: ave = cur
 
-        # if i % 100 == 0: print 'Computed up to %d/%d' % (i, n)
+        if verbose and i % 100 == 0: print 'Computed %d/%d steps' % (i, n)
 
+    # plot
     t = dt*ntspd*np.arange(n)  # DPD time
-    save_res('result.txt', (t, th, om, a, b, c, el, a_, c_))
-    plt.plot(t, th, 'r-', label='theta')
-    plt.plot(t, om, 'b-', label='omega')
-    plt.plot([t[si], t[si]], [-180, 180], 'k--')
-    plt.legend()
+    save_txt('result.txt', (t, th, om, a, b, c, el, a_, b_, c_))
+
+    plot(t, th, 'r-', label='theta')
+    plot(t, om, 'b-', label='omega')
+    plot([t[si], t[si]], [-180, 180], 'k--')
+    legend()
     savefig('angle.png')
-    plt.close()
-    plt.plot(t, a, 'r-', label='a')
-    plt.plot(t, b, 'g-', label='b')
-    plt.plot(t, c, 'b-', label='c')
-    plt.plot([t[si], t[si]], [0, 2], 'k--')
-    plt.legend()
+    close()
+
+    plot(t, a, 'r-', label='a')
+    plot(t, b, 'g-', label='b')
+    plot(t, c, 'b-', label='c')
+    plot([t[si], t[si]], [0, 2], 'k--')
+    legend()
     savefig('diam.png')
-    plt.close()
-    plt.plot(t, el, 'r-', label='ellipticity')
-    plt.plot([t[si], t[si]], [0, 100], 'k--')
-    plt.legend()
+    close()
+
+    plot(t, el, 'r-', label='ellipticity')
+    plot([t[si], t[si]], [0, 100], 'k--')
+    legend()
     savefig('ellipticity.png')
-    plt.close()
+    close()
 
     # compute means and stds
     a,  au  = np.mean( a[si:]), np.std( a[si:])
@@ -149,13 +160,14 @@ def process_data(plydir, dt, ntspd, sh):
     el, elu = np.mean(el[si:]), np.std(el[si:])
     th, thu = np.mean(th[si:]), np.std(th[si:])
     a_, au_ = np.mean(a_[si:]), np.std(a_[si:])
+    b_, bu_ = np.mean(b_[si:]), np.std(b_[si:])
     c_, cu_ = np.mean(c_[si:]), np.std(c_[si:])
     fr, fru = get_fr(t[si:], om[si:]); fr *= 2.*np.pi/sh; fru /= sh
 
     with open('post.txt', 'w') as f:
         f.write('# fr\tfru\ta\tau\tb\tbu\tc\tcu\tth\tthu\tel\telu\ta_\tau_\tc_\tcu_\n')
-        f.write('  %.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\n' %
-                (  fr,   fru,  a,    au,   b,    bu,   c,    cu,   th,   thu,  el,   elu,  a_,   au_,  c_,   cu_))
+        f.write('  %.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\n' %
+                (  fr,   fru,  a,    au,   b,    bu,   c,    cu,   th,   thu,  el,   elu,  a_,   au_,  b_,   bu_,  c_,   cu_))
 
 
 if __name__ == '__main__':
