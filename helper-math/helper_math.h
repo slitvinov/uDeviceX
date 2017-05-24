f746f9ee00867a5d526ca5395dd6c42bb385fbab
Sergey Litvinov <slitvinov@gmail.com>
2017-05-24 12:33:45 +0200

    Clean up helper-math.h (now it the same as in master)

--- helper-math/helper_math.h
+++ helper-math/helper_math.h
@@ -1,36 +1,4 @@
-/**
- * Copyright 1993-2013 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
-
-/*
- *  This file implements common mathematical operations on vector types
- *  (float3, float4 etc.) since these are not provided as standard by CUDA.
- *
- *  The syntax is modeled on the Cg standard library.
- *
- *  This is part of the Helper library includes
- *
- *    Thanks to Linh Hah for additions and fixes.
- */
-
-////////////////////////////////////////////////////////////////////////////////
-// negate
-////////////////////////////////////////////////////////////////////////////////
-inline __host__ __device__ float3 operator-(float3 &a)
-{
-  return make_float3(-a.x, -a.y, -a.z);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// addition
-////////////////////////////////////////////////////////////////////////////////
+/** Copyright 1993-2013 NVIDIA Corporation.  All rights reserved. **/
 
 inline __host__ __device__ float3 operator+(float3 a, float3 b)
 {
@@ -43,9 +11,6 @@ inline __host__ __device__ void operator+=(float3 &a, float3 b)
   a.z += b.z;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// subtract
-////////////////////////////////////////////////////////////////////////////////
 inline __host__ __device__ float3 operator-(float3 a, float3 b)
 {
   return make_float3(a.x - b.x, a.y - b.y, a.z - b.z);
@@ -56,11 +21,7 @@ inline __host__ __device__ float3 operator*(float b, float3 a)
   return make_float3(b * a.x, b * a.y, b * a.z);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// dot product
-////////////////////////////////////////////////////////////////////////////////
 inline __host__ __device__ float dot(float3 a, float3 b)
 {
   return a.x * b.x + a.y * b.y + a.z * b.z;
 }
-
