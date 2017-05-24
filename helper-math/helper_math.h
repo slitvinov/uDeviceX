f167c1b98145e462b977fe55562cffb199f98d0d
Sergey Litvinov <slitvinov@gmail.com>
2017-05-24 13:41:47 +0200

    Convert to double in helper_math

--- helper-math/helper_math.h
+++ helper-math/helper_math.h
@@ -3,53 +3,39 @@
 #define IHD inline __host__ __device__
 #define mf3(a, b, c) make_float3((a), (b), (c))
 
-IHD double dmult (double a, double b)           { return a * b; }
-IHD double dplus (double a, double b)           { return a + b; }
-IHD double dplus3(double a, double b, double c) { return a + b + c; }
-IHD double dminus(double a, double b)           { return a - b; }
-
-IHD float3 operator+(float3 a, float3 b)
-{
-  return mf3(a.x + b.x, a.y + b.y, a.z + b.z);
+IHD float3 operator+(float3 a, float3 b) {
+  double ax = a.x, ay = a.y, az = a.z;
+  double bx = b.x, by = b.y, bz = b.z;
+  return mf3(ax + bx, ay + by, az + bz);
 }
 
-IHD void operator+=(float3 &a, float3 b)
-{
+IHD void operator+=(float3 &a, float3 b) {
   a.x += b.x;
   a.y += b.y;
   a.z += b.z;
 }
 
-IHD float3 operator-(float3 a, float3 b)
-{
-  return mf3(a.x - b.x, a.y - b.y, a.z - b.z);
+IHD float3 operator-(float3 a, float3 b) {
+  double ax = a.x, ay = a.y, az = a.z;
+  double bx = b.x, by = b.y, bz = b.z;
+  return mf3(ax - bx, ay - by, az - bz);
 }
 
-IHD float3 operator*(double b, float3 a)
-{
-  return mf3(b * a.x, b * a.y, b * a.z);
+IHD float3 operator*(double b, float3 a) {
+  double ax = a.x, ay = a.y, az = a.z;
+  return mf3(b * ax, b * ay, b * az);
 }
 
-IHD double dot(float3 a, float3 b)
-{
-  return \
-    dmult(a.x, b.x) +
-    dmult(a.y, b.y) +
-    dmult(a.z, b.z);
+IHD double dot(float3 a, float3 b) {
+  double ax = a.x, ay = a.y, az = a.z;
+  double bx = b.x, by = b.y, bz = b.z;
+  return ax*bx + ay*by + az*bz;
 }
 
 IHD float3 cross(float3 a, float3 b) {
   double ax = a.x, ay = a.y, az = a.z;
   double bx = b.x, by = b.y, bz = b.z;
-
   return mf3(ay*bz - az*by, az*bx - ax*bz, ax*by - ay*bx);
-}
-
-/* yy := a*xx + yy */
-IHD void axpy(double a, float3 xx, float3 &yy) {
-  yy.x += a * xx.x;
-  yy.y += a * xx.y;
-  yy.z += a * xx.z;
 }
 
 #undef mf3
