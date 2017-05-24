4495cdb512a974078f09c5b7510efbb2467efcb0
Sergey Litvinov <slitvinov@gmail.com>
2017-05-24 13:29:58 +0200

    Use abbreviates

--- helper-math/helper_math.h
+++ helper-math/helper_math.h
@@ -1,38 +1,46 @@
 /** Copyright 1993-2013 NVIDIA Corporation.  All rights reserved. **/
-inline __host__ __device__ double dmult (double a, double b)           { return a * b; }
-inline __host__ __device__ double dplus (double a, double b)           { return a + b; }
-inline __host__ __device__ double dplus3(double a, double b, double c) { return a + b + c; }
-inline __host__ __device__ double dminus(double a, double b)           { return a - b; }
 
-inline __host__ __device__ float3 operator+(float3 a, float3 b)
+#define IHD inline __host__ __device__
+#define mf3(a, b, c) make_float3((a), (b), (c))
+
+IHD double dmult (double a, double b)           { return a * b; }
+IHD double dplus (double a, double b)           { return a + b; }
+IHD double dplus3(double a, double b, double c) { return a + b + c; }
+IHD double dminus(double a, double b)           { return a - b; }
+
+IHD float3 operator+(float3 a, float3 b)
 {
-  return make_float3(a.x + b.x, a.y + b.y, a.z + b.z);
+  return mf3(a.x + b.x, a.y + b.y, a.z + b.z);
 }
-inline __host__ __device__ void operator+=(float3 &a, float3 b)
+
+IHD void operator+=(float3 &a, float3 b)
 {
   a.x += b.x;
   a.y += b.y;
   a.z += b.z;
 }
 
-inline __host__ __device__ float3 operator-(float3 a, float3 b)
+IHD float3 operator-(float3 a, float3 b)
 {
-  return make_float3(a.x - b.x, a.y - b.y, a.z - b.z);
+  return mf3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
-inline __host__ __device__ float3 operator*(double b, float3 a)
+IHD float3 operator*(double b, float3 a)
 {
-  return make_float3(b * a.x, b * a.y, b * a.z);
+  return mf3(b * a.x, b * a.y, b * a.z);
 }
 
-inline __host__ __device__ double dot(float3 a, float3 b)
+IHD double dot(float3 a, float3 b)
 {
   return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
 /* yy := a*xx + yy */
-inline __host__ __device__ void axpy(double a, float3 xx, float3 &yy) {
+IHD void axpy(double a, float3 xx, float3 &yy) {
   yy.x += a * xx.x;
   yy.y += a * xx.y;
   yy.z += a * xx.z;
 }
+
+#undef mf3
+#undef IHD
