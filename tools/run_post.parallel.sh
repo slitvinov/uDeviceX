7293132d0873752fdb94207bc155b05ed243174d
lina-kulakova <lina.kulakova@mavt.ethz.ch>
2017-06-22 21:09:16 +0200

    Fix wrong parameter name

--- tools/run_post.parallel.sh
+++ tools/run_post.parallel.sh
@@ -6,7 +6,7 @@ m1=$2 # M for shear 1
 (
 cd $d
 dt=`awk '$1 == "dt"             {print $2}' params.txt`
-sh=`awk '$1 == "_gamma_dot"     {print $2}' params.txt`
+sh=`awk '$1 == "gamma_dot"      {print $2}' params.txt`
 st=`awk '$1 == "steps_per_dump" {print $2}' params.txt`
 gc=`awk '$1 == "RBCgammaC"      {print $2}' params.txt`
 m2=`sh get_sc.sh diag.txt`
