e8506927d32ffc90c90a0644ae2311349f4979da
Sergey Litvinov <slitvinov@gmail.com>
2017-06-27 07:52:02 +0200

    test -> tests

--- mpi-dpd/tests/rbc.sh
+++ mpi-dpd/tests/rbc.sh
@@ -13,7 +13,7 @@
 # cp sdf/wall1/wall.dat sdf.dat
 # x=6 y=6 z=6; echo 1 0 0 $x  0 1 0 $y  0 0 1 $z  0 0 0 1 > rbcs-ic.txt
 # cp .rbc.rbc.h params/rbc.inc0.h
-# cp .conf.rbc.h .conf.h
+# cp conf/rbc.h .conf.h
 # rm -rf ply h5 diag.txt
 # (make clean && make -j && make -C ../tools) > /dev/null
 #  ./test 
